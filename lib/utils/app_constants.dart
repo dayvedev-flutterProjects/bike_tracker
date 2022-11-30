7eb7304b001a6f9c9d0a66069353e5941bfbe541
dayvedev <dayvedev@gmail.com>
2022-11-30 21:45:11 +0000

    Create app_constants.dart

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,9 @@
+
+class AppConstants{
+
+  //Hive db constants
+  static const bikeBox = "bikes";
+  static final String mySharedPrefsBox = "sharedPreferences";
+  static final String vehicleBox = "vehicles";
+
+}
